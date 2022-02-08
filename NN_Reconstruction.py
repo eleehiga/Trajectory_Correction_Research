2f25ef702660bd3c4ec756a6e348a138b6885438
Ethan Higa <eleehiga808@gmail.com>
2022-02-08 10:07:07 -0800

    commented NN_Recostruction code

--- NN_Reconstruction.py
+++ NN_Reconstruction.py
@@ -9,33 +9,28 @@ from numpy import array
 
 # construct LSTM neural network
 def apply_nn(trajectory):
-    start_predict = len(trajectory)
+    start_predict = len(trajectory) # start predicting there there are None in data
     for i in range(len(trajectory)):
         if(trajectory[i][0] is None):
             start_predict = i
             break
     end_predict = len(trajectory)
-    for i in range(start_predict,len(trajectory)):
+    for i in range(start_predict,len(trajectory)): # end predicting when not None anymore
         if(not(trajectory[i][0] is None)):
             end_predict = i
             break
     # scale data between 0 and 1
-    cut_traj = trajectory[0:start_predict]
-    scaler=MinMaxScaler(feature_range=(0,1))
-    scaled_traj = scaler.fit_transform(np.array(cut_traj).reshape(-1,2))
+    cut_traj = trajectory[0:start_predict] # trajectory before predicting to use as training
+    scaler=MinMaxScaler(feature_range=(0,1)) # will scale data between 0 and 1
+    scaled_traj = scaler.fit_transform(np.array(cut_traj).reshape(-1,2)) 
     # make all X_train because just use that to predict points
     train_data = scaled_traj
-    print('train_data.shape: ')
-    print(train_data.shape)
 
-
-    time_step=5
+    time_step=5 # how many points previously to use to predict
     X_train, y_train = create_dataset(train_data, time_step)
-    print('X_train.shape: ')
-    print(X_train.shape) # TODO remove print
 
     # reshape input to be [samples, time steps, features] which is required for LSTM
-    units = 3
+    units = 3 # for nodes in network
     model = Sequential()
     model.add(LSTM(units, input_shape=(time_step,2), return_sequences=True)) # no activation as we are not returning a binary value
     model.add(LSTM(units, return_sequences=True))
@@ -54,39 +49,28 @@ def apply_nn(trajectory):
 
     model.fit(X_train,
           y_train,
-          epochs=100
+          epochs=5000
           ) 
 
-    x_input = train_data[train_data.shape[0]-time_step:] # maybe reshape(2,-1)?
+    x_input = train_data[train_data.shape[0]-time_step:] # last time_step points
     temp_input = list(x_input)
-    corrected_traj = trajectory
+    corrected_traj = trajectory # will replace null points
     i = 0
     while(i < end_predict - start_predict):
-        if(len(temp_input)>time_step):
-            #print(temp_input)
-            #print('x_input.shape: ')
-            #print(x_input.shape)
-            x_input=np.array(temp_input[1:])
-            #x_input=x_input.reshape(1,-1)
+        if(len(temp_input)>time_step): # will always be time_step + 1
+            x_input=np.array(temp_input[1:]) # will remove one so its amount is still time_step
             x_input = x_input.reshape((1, time_step, 2))
-            print(x_input)
             yhat = model.predict(x_input, verbose=0)
-            temp_input.extend(yhat.tolist())
+            temp_input.extend(yhat.tolist()) # append to the end
             temp_input=temp_input[1:]
-            #print(temp_input)
             corrected_traj[start_predict + i] = scaler.inverse_transform(yhat).tolist()[0]
             i=i+1
-        else:
+        else: # when len is time_step predict one more and add that on to the temp_input list
             x_input = x_input.reshape((1, time_step, 2))
             yhat = model.predict(x_input, verbose=0)
-            print(yhat[0])
             temp_input.extend(yhat.tolist())
-            #print('temp_input.shape: ')
-            #print(np.array(temp_input).shape)
             corrected_traj[start_predict + i] = scaler.inverse_transform(yhat).tolist()[0]
             i=i+1
-    print('corrected_traj')
-    print(corrected_traj)
     return corrected_traj
 
 
